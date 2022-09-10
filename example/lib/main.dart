fd65aae4c512efcac95fed4aa666f97f03055f06
Jim Tompkins <jim.tompkins@gmail.com>
2022-09-10 13:24:21 -0300

    First commit

---
+++ example/lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bass/flutter_bass.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    // BASS_Init: -1 = default device, 48000 = sample rate, 0 = flags
+    bass.BASS_Init(-1, 48000, 0, nullptr, nullptr);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(fontSize: 25);
+    const spacerSmall = SizedBox(height: 10);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Native Packages'),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                const Text(
+                  'This calls a native function through FFI that is shipped as source in the package. '
+                  'The native code is built as part of the Flutter Runner build.',
+                  style: textStyle,
+                  textAlign: TextAlign.center,
+                ),
+                spacerSmall,
+                Text(
+                  'placeholder',
+                  //              'sum(1, 2) = $sumResult',
+                  style: textStyle,
+                  textAlign: TextAlign.center,
+                ),
+                spacerSmall,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
